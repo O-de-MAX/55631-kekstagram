dd5315fb50e496975ec0468a77e59c44825c7191
Maria Patrusheva <maria.patrusheva@gmail.com>
2016-08-09 13:36:27 +0500

    Задание 7. Добавлен оверлей

--- build/js/resizer.js
+++ build/js/resizer.js
@@ -119,6 +119,22 @@
           this._resizeConstraint.side - this._ctx.lineWidth / 2,
           this._resizeConstraint.side - this._ctx.lineWidth / 2);
 
+      this._ctx.fillStyle = 'rgba(0, 0, 0, 0.8)';
+      this._ctx.beginPath();
+      this._ctx.moveTo((-this._container.width / 2), (-this._container.height / 2));
+      this._ctx.lineTo((this._container.width / 2), (-this._container.height / 2));
+      this._ctx.lineTo((this._container.width / 2), (this._container.height / 2));
+      this._ctx.lineTo((-this._container.width / 2), (this._container.height / 2));
+      this._ctx.moveTo((-this._resizeConstraint.side / 2 - this._ctx.lineWidth), (-this._resizeConstraint.side / 2 - this._ctx.lineWidth));
+      this._ctx.lineTo((this._resizeConstraint.side / 2 - this._ctx.lineWidth / 2), (-this._resizeConstraint.side / 2 - this._ctx.lineWidth));
+      this._ctx.lineTo((this._resizeConstraint.side / 2 - this._ctx.lineWidth / 2), (this._resizeConstraint.side / 2 - this._ctx.lineWidth / 2));
+      this._ctx.lineTo((-this._resizeConstraint.side / 2 - this._ctx.lineWidth), (this._resizeConstraint.side / 2 - this._ctx.lineWidth / 2));
+      this._ctx.fill('evenodd');
+
+      this._ctx.fillStyle = '#ffffff';
+      this._ctx.textAlign = 'center';
+      this._ctx.fillText('Тут будет размер', 0, (-this._resizeConstraint.side / 2 - 2 * this._ctx.lineWidth));
+
       // Восстановление состояния канваса, которое было до вызова ctx.save
       // и последующего изменения системы координат. Нужно для того, чтобы
       // следующий кадр рисовался с привычной системой координат, где точка
@@ -126,6 +142,7 @@
       // некорректно сработает даже очистка холста или нужно будет использовать
       // сложные рассчеты для координат прямоугольника, который нужно очистить.
       this._ctx.restore();
+
     },
 
     /**
