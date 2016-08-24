e7cf44a87a5a0457fadf5ba8d47c4f31675ec3f8
Maria Patrusheva <maria.patrusheva@gmail.com>
2016-08-24 14:17:31 +0500

    Инициализация списка фотографий

--- src/pictures.js
+++ src/pictures.js
@@ -1,4 +1,11 @@
 'use strict';
+(function() {
+
+var filterElements = document.querySelector('.filters');
+function hideFilters() {
+  filterElements.classList.add('hidden');
+}
+hideFilters();
 
 function loadJSONP(url, callback) {
 
@@ -6,7 +13,6 @@ function loadJSONP(url, callback) {
   var newScript = document.createElement('script');
   newScript.src = url;
   document.body.appendChild(newScript);
-
 
   window.JSONPCallback = function(data) {
     callback(data);
@@ -19,3 +25,37 @@ loadJSONP('/api/pictures', function(data) {
   pictures = data;
   console.log(pictures);
 });
+
+var picturesContainer = document.querySelector('.pictures');
+var templateElement = document.getElementById('picture-template');
+var elementToClone;
+
+if ('content' in templateElement) {
+  elementToClone = templateElement.content.querySelector('.picture');
+} else {
+  elementToClone = templateElement.querySelector('.picture');
+}
+
+var getPicturesElement = function(data, container) {
+  var element = elementToClone.cloneNode(true);
+  container.appendChild(element);
+
+  var picturesElement = new Image();
+
+  picturesElement.onload = function() {
+    templateElement.content.querySelector('img').src = data.url;
+  };
+  picturesElement.onerror = function() {
+    element.classList.add('picture-load-failure');
+
+  picturesElement.src = data.url;
+
+  return element;
+};
+
+pictures.forEach(function(picture) {
+  getPicturesElement(picture, picturesContainer);
+});
+
+
+})();
