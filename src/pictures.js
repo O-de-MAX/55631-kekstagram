28408e886d0b7e097e8c0e7691f68cd3ef215c86
Maria Patrusheva <maria.patrusheva@gmail.com>
2016-08-25 14:51:58 +0500

    Исправления в функциях шаблонизации + добавлена функция показа фильтров

--- src/pictures.js
+++ src/pictures.js
@@ -1,61 +1,67 @@
 'use strict';
 
-var filterElements = document.querySelector('.filters');
+(function() {
+  var filterElements = document.querySelector('.filters');
 
-var picturesContainer = document.querySelector('.pictures');
-var template = document.getElementById('picture-template');
+  var picturesContainer = document.querySelector('.pictures');
+  var template = document.getElementById('picture-template');
 
-var elementToClone;
-if ('content' in template) {
-  elementToClone = template.content.querySelector('.picture');
-} else {
-  elementToClone = template.querySelector('.picture');
-}
+  var elementToClone;
+  if ('content' in template) {
+    elementToClone = template.content.querySelector('.picture');
+  } else {
+    elementToClone = template.querySelector('.picture');
+  }
 
-var pictures = [];
-
-
-function hideFilters() {
-  filterElements.classList.add('hidden');
-}
-
-function loadJSONP(url, callback) {
-  url += '?callback=JSONPCallback';
-  var newScript = document.createElement('script');
-  newScript.src = url;
-  document.body.appendChild(newScript);
-
-  window.JSONPCallback = function(data) {
-    callback(data);
-  };
-}
-
-var getPicturesElement = function(picture) {
-  var pictureElement = elementToClone.cloneNode(true);
-  var imageElement = new Image();
-
-  imageElement.onload = function() {
-    template.content.querySelector('img').src = picture.url;
-  };
-  imageElement.onerror = function() {
-    pictureElement.classList.add('picture-load-failure');
-  };
-  imageElement.src = picture.url;
-
-  return pictureElement;
-};
+  var pictures = [];
 
 
-var renderPictures = function() {
-  pictures.forEach(function(picture) {
-    picturesContainer.appendChild(getPicturesElement(picture));
+  function hideFilters() {
+    filterElements.classList.add('hidden');
+  }
+
+  function showFilters() {
+    filterElements.classList.remove('hidden');
+  }
+
+  function loadJSONP(url, callback) {
+    url += '?callback=JSONPCallback';
+    var newScript = document.createElement('script');
+    newScript.src = url;
+    document.body.appendChild(newScript);
+
+    window.JSONPCallback = function(data) {
+      callback(data);
+    };
+  }
+
+  function getPicturesElement(picture) {
+    var pictureElement = elementToClone.cloneNode(true);
+    var imageElement = new Image();
+
+    imageElement.onload = function() {
+      template.content.querySelector('img').src = picture.url;
+    };
+    imageElement.onerror = function() {
+      pictureElement.classList.add('picture-load-failure');
+    };
+    imageElement.src = picture.url;
+
+    return pictureElement;
+  }
+
+  function renderPictures() {
+    pictures.forEach(function(picture) {
+      picturesContainer.appendChild(getPicturesElement(picture));
+    });
+  }
+
+  hideFilters();
+
+  loadJSONP('/api/pictures', function(data) {
+    pictures = data;
+    console.log(pictures);
+    renderPictures();
+    showFilters();
   });
-};
-
-
-hideFilters();
-
-loadJSONP('/api/pictures', renderPictures, function(data) {
-  pictures = data;
-  console.log(pictures);
 })();
