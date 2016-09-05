8c06b52888d87b738829bd65966897c4a7811117
Maria Patrusheva <maria.patrusheva@gmail.com>
2016-09-05 15:49:20 +0500

    Обрабочик события scroll

--- src/pictures.js
+++ src/pictures.js
@@ -2,15 +2,16 @@
 
 var filterElements = document.querySelector('.filters');
 var picturesContainer = document.querySelector('.pictures');
+var footer = document.querySelector('footer');
+
+var pageNumber = 0;
+var pageSize = 11;
 
 var pictures = [];
 
 var load = require('./load');
 var Picture = require('./picture');
 var gallery = require('./gallery.js');
-
-var pageNumber = 0;
-var pageSize = 11;
 
 
 function hideFilters() {
@@ -29,6 +30,12 @@ function renderPictures() {
 }
 
 hideFilters();
+
+window.addEventListener('scroll', function() {
+  if (footer.getBoundingClientRect().bottom - window.innerHeight <= 0) {
+    load(pageNumber++);
+  }
+});
 
 load('/api/pictures', {
   from: pageNumber,
